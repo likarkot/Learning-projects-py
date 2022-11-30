a01c9bfae55c66a3ab9c583928ef30d9f19febc0
likarkot <118125738+likarkot@users.noreply.github.com>
2022-11-30 16:37:12 +0200

    Delete Fragmentation.py

--- Fragmentation.py
+++
@@ -1,7 +0,0 @@
-def chunk(ishod_list, size):
-    return [ishod_list[i:i + size] for i in range(0, len(ishod_list), size)]
-
-ishod_list = ['q', 'w', 'e', 'r', 't', 'y']
-print(chunk(ishod_list, 2))
-# Вывод:
-[['q', 'w'], ['e', 'r'], ['t', 'y']]
